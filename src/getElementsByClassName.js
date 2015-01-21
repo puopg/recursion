ed0b170e457616bea7aaa9c23c01fb38f57f3069
puopg <michelchen101@gmail.com>
2015-01-20 20:58:33 -0800

    Add getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  // Start at root node
+  var elements = [];
+  searchForClassInNode(className, document.body);
+  return elements;
+
+  /// Function: searchForClassInNode(className, node)
+  /// This function will look for elements under the given node with classtype "className"
+  function searchForClassInNode(className, node) {
+  	if(node.nodeType === 1) { // An element Node
+  	  	if(node.classList.contains(className)) {
+  			elements.push(node);
+  		}
+  	}
+  	for(var i = 0; i < node.childNodes.length; i++) {
+  		searchForClassInNode(className, node.childNodes[i]);
+	}
+  }
+
 };
